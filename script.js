dc0e0a93b28283bee53fb044fe26318afbb62e9d
PaktamXbotz <107012758+PaktamXbotz@users.noreply.github.com>
2025-03-01 14:10:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const spans = document.querySelectorAll('.love-shape span');
+    const spans = document.querySelectorAll('.tulip-shape span');
     
     spans.forEach((span, index) => {
         span.style.animationDelay = `${index * 0.1}s`;
     });
 
-    document.querySelector('.love-shape').addEventListener('mouseover', () => {
+    document.querySelector('.tulip-shape').addEventListener('mouseover', () => {
         spans.forEach(span => {
             span.style.transform = `scale(1.2)`;
             span.style.transition = 'transform 0.5s';
         });
     });
 
-    document.querySelector('.love-shape').addEventListener('mouseout', () => {
+    document.querySelector('.tulip-shape').addEventListener('mouseout', () => {
         spans.forEach(span => {
             span.style.transform = `scale(1)`;
             span.style.transition = 'transform 0.5s';
